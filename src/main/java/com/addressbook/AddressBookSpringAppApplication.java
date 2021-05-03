7a847c0c1c9a9d8488d5824ad99a65dbfd544295
snehjeetc12 <csnehjeet12@gmail.com>
2021-05-03 21:54:17 +0530

    Added logging configuration in application properties

--- src/main/java/com/addressbook/AddressBookSpringAppApplication.java
+++ src/main/java/com/addressbook/AddressBookSpringAppApplication.java
@@ -3,14 +3,17 @@ package com.addressbook;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import lombok.extern.slf4j.Slf4j;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @EnableSwagger2
 @SpringBootApplication
+@Slf4j
 public class AddressBookSpringAppApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AddressBookSpringAppApplication.class, args);
+		log.info("Employee payroll app started successfully");
 	}
 
 }
