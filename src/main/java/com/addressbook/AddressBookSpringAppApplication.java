7262ddb909b34002ea0410615622642df360645e
snehjeetc12 <csnehjeet12@gmail.com>
2021-05-03 23:58:11 +0530

    Added configuration for different environments

--- src/main/java/com/addressbook/AddressBookSpringAppApplication.java
+++ src/main/java/com/addressbook/AddressBookSpringAppApplication.java
@@ -2,6 +2,7 @@ package com.addressbook;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 import lombok.extern.slf4j.Slf4j;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -12,8 +13,8 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 public class AddressBookSpringAppApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(AddressBookSpringAppApplication.class, args);
-		log.info("Employee payroll app started successfully");
+		ConfigurableApplicationContext context = SpringApplication.run(AddressBookSpringAppApplication.class, args);
+		log.info("profile: {}", context.getEnvironment().getProperty("profile"));
 	}
 
 }
